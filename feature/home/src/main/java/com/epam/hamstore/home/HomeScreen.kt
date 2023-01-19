6914749e59a5b4d93976fa2dc17ec0a50fe5f5d6
ivan_khomiak <ivan_khomiak@epam.com>
2023-01-19 18:09:54 +0200

    Fix ktlint and detekt issues 6.

--- feature/home/src/main/java/com/epam/hamstore/home/HomeScreen.kt
+++ feature/home/src/main/java/com/epam/hamstore/home/HomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 internal fun HomeRoute() {
